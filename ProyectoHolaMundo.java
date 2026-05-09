0584ceb7d503c10e7c19b393d0e7ce9d630272cd
VynJs <joshbel31@gmail.com>
2026-05-08 18:41:00 -0600

    Changing outputs

--- ProyectoHolaMundo.java
+++ ProyectoHolaMundo.java
@@ -1,5 +1,7 @@
 public class ProyectoHolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, Mundo!");
+        System.out.println("¡Bienvenidos a Java!");
+        System.out.println("Y a Git/GitHub!");
     }
 }
